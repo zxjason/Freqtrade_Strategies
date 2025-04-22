6a051696f135aaa287d2a4be5987290bdf3d739f
ZxJaSoN <zxjason1984@gmail.com>
2025-04-22 11:11:53 -0400

    Update NASOSv5_jmod1.py
    commit changes

--- NASOSv5/NASOSv5_jmod1.py
+++ NASOSv5/NASOSv5_jmod1.py
@@ -1,1 +1,13 @@
-# zxjason
+#############################################################################################################
+##    NASOSv5 Strategy by @zxjason                                                                         ##
+##    https://github.com/zxjason/Freqtrade_Strategies                                                      ##
+##    Strategy for Freqtrade https://github.com/freqtrade/freqtrade                                        ##
+#############################################################################################################
+##    GENERAL RECOMMENDATIONS                                                                              ##
+##    For optimal performance, suggested to use only BTC and ETH.                                          ##
+#############################################################################################################
+##    DONATIONS                                                                                            ##
+##    usdc: 0xedB85Ce16ba8268F32925f6fF48190Cea97a9b81                                                     ##
+##    REFERRAL LINKS                                                                                       ##
+#############################################################################################################
+
